3bb566e01819eb44173f8136a471c5abb914c178
Tor Arne Helgesen <47104967+torarnehelgesen@users.noreply.github.com>
2023-10-10 12:43:42 +0200

    Update posten-utils.ts

--- src/posten-utils.ts
+++ src/posten-utils.ts
@@ -52,10 +52,9 @@ export const deliveryDayText = (idx: number, deliveryDay: Moment, formattedDate:
     return formattedDate || 'N/A';
   }
 };
-//['2023-10-09', '2023-10-11', '2023-10-13', '2023-10-17', '2023-10-19', '2023-10-23']
+//['2023-10-09', '2023-10-11', '2023-10-13', '2023-10-17', '2023-10-19', '2023-10-23'] //  console.write('test');
 const parseDeliveryDay = (deliveryDay: string, locale: string, config: PostenCardConfig): DeliveryDay => {
   const dateFormat = !config.date_format || config.date_format.trim() === '' ? defaultDateFormat : config.date_format;
-//  console.write('test');
   const segments = deliveryDay.split('-');
   const deliveryDayMoment = moment().month(months[segments[1]]).calendar(segments[2]);
   const formattedDate = dateUtils.formatDate(locale, dateFormat, deliveryDayMoment).capitalize();
